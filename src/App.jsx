cc7fb63d3033a9f253f979ea4c702f1e9e93ed11
Parv <parvruhil68@gmail.com>
2024-11-05 21:50:20 +0530

    jsx files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,68 @@
-
+import { useEffect } from "react";
+import { useState } from "react"
 
 function App() {
-  
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleCreateRandomHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor); 
+  }
+
+  function handleCreateRandomRgbColor() {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+    setColor(`rgb(${r}, ${g}, ${b})`); 
+  }
+
+  useEffect(() => {
+    if (typeOfColor === 'rgb') {
+      handleCreateRandomRgbColor();
+    } else {
+      handleCreateRandomHexColor();
+    }
+  }, [typeOfColor]); 
 
   return (
     <>
-      
+      <div className="container"
+        style={{
+          backgroundColor: color,
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "start",
+          color: "white",
+        }}
+      >
+        <button onClick={() => setTypeOfColor('hex')}>Create HEX Color</button>
+        <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
+        <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
+      </div>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "2rem",
+        marginTop: "50px",
+        color: "white",
+      }}>
+        <h3>{typeOfColor === 'rgb' ? 'RGB Color' : 'HEX Color'}</h3>
+        <h1>{color}</h1>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
